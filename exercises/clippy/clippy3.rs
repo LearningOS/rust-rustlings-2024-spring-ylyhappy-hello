42dc37829426e17cee067e9ba629b3f71e152c36
ylyhappy <2778335106@qq.com>
2024-04-28 14:10:48 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -6,12 +6,6 @@
 
 #[allow(unused_variables, unused_assignments)]
 fn main() {
-    let my_option: Option<()> = None;
-    if my_option.is_none() {
-        return
-    }
-    my_option.unwrap();
-
     let my_arr = &[
         -1, -2, -3,
         -4, -5, -6
